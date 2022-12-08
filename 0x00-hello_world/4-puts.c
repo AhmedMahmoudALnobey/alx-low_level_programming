7ebbbb3902897765dd84ae8e9d888e981c9f1dc8
AhmedMahmoudALnobey <ahmdaln073@gmail.com>
2022-12-08 07:34:34 -0800

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
+#include<iostream>
 /**
-Description: main - Prints out a string to stdout.
-Return: 0 if success.
+hello
 */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);  
+int main(void){
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0) ;
 }
