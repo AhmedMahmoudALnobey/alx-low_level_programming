2276c0bb24e4e856a84b071d2da71b921a5e4559
AhmedMahmoudALnobey <ahmdaln073@gmail.com>
2022-12-16 14:02:33 -0800

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main/3-main.c
@@ -0,0 +1,25 @@
+#include "../main.h"
+
+
+
+/**
+
+ * main - check the code for ALX School students.
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+
+
+int main(void)
+  
+{
+  
+  print_numbers();
+  
+  return (0);
+  
+}
